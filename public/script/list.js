e49344e2d418f168f18cb0a5b99a920e7e5b8760
viveksingh2310 <viveksingh14538@gmail.com>
2024-08-07 13:09:34 +0530

    feat list editing
    the edit icon and its view is modified with addition of a sample model in .github directory

--- public/script/list.js
+++ public/script/list.js
@@ -1,88 +1,87 @@
-console.log('hello you are qwwwwwwwwwwwwwwww')
+console.log('hello hte list.js is running successfully')
 $("#add-new-task").focus();
-$(".sub-btn").click(function(ve){
-if(document.getElementById('add-new-task').value!=""){
-fetch('/list',{method:'PUT',
-    headers:{ 'Content-Type': 'application/json'},
-    body:JSON.stringify({
-        "title":returnAndEmptyInput()
-    })
-})
-}
+$(".sub-btn").click(function (ve) {
+    if (document.getElementById('add-new-task').value != "") {
+        fetch('/list', {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                "title": returnAndEmptyInput()
+            })
+        })
+    }
 });
- async function isImportant(id){
-    const result=await fetch('/list/imp',{method:'PUT',
-         headers:{ 'Content-Type': 'application/json'},
-         body:JSON.stringify({
-             "id":id
-         })
-     }).then((response)=>{
-     return response.json();
-     }).catch((data)=>{
-         console.log(data);
-     });
-    //  console.log('vivek'+JSON.stringify(result))
-     return result.response;
- }
- function getID(ele){
-    const hoveredLabel=$(ele).siblings('class'=='form-check-input').attr('id');
-    const id=hoveredLabel.replace('flexCheckDefault','');
-return id
- }
-//  function whetherImp()
-$('.form-check-label').hover( async function(){
-    //getting the id
-    // const hoveredLabel=$(this).siblings('class'=='form-check-input').attr('id');
-    // const id=hoveredLabel.replace('flexCheckDefault','');
-    const id=getID(this);
-    const editButton=$("<span><button id='edit-btn' >Edit</button></span>")
-    // console.log('hellllllllooo birch'+await isImportant(id));
-    let text='';
-    const res=await isImportant(id);
-    if(res=='true')
-        text='remove imp'
-    else text='make imp'
-    console.log('the mf textis:'+text);
-    const impButton=$("<span><button id='imp-btn'> "+" "+text+" "+" </button></span>")
+async function isImportant(id) {
+    const result = await fetch('/list/imp', {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            "id": id
+        })
+    }).then((response) => {
+        return response.json();
+    }).catch((data) => {
+        console.log(data);
+    });
+    return result.response;
+}
+function getID(ele) {
+    const hoveredLabel = $(ele).siblings('class' == 'form-check-input').attr('id');
+    const id = hoveredLabel.replace('flexCheckDefault', '');
+    return id
+}
+function getTaskDetails(id){
+
+}
+$('.form-check-label').hover(async function () {
+    const id = getID(this);
+    const editButton = $("<span><button id='edit-btn' >Edit</button></span>")
+    let text = '';
+    const res = await isImportant(id);
+    if (res == 'true')
+        text = 'remove imp'
+    else text = 'make imp'
+    console.log('the mf textis:' + text);
+    const impButton = $("<span><button id='imp-btn'> " + " " + text + " " + " </button></span>")
     $(this).append(impButton);
     $(this).append(editButton);
-    $('.edit-btn').on('click',(function(e){
-        console.log('edit is clicked at '+id);
 
+
+
+
+    $('#edit-btn').on('click', (function (e) {
+///here you have to make changes
+        const editTitle=document.getElementById('edit-title');
+editTitle.value='dsjfdjfjfdj';
+        $('#edit-div').toggle();
+        console.log('edit button is clicked at ' + id);
     }));
-    $('#imp-btn').on('click',function(e){
-        // if(res=='true')
-        //     $('#imp-btn').text('remove imp')
-        // else
-        // $('#imp-btn').text('make imp')
-        fetch('/list/imp',{method:'POST',
-            headers:{ 'Content-Type': 'application/json'},
-            body:JSON.stringify({
-                "id":id
+
+
+
+
+    $('#imp-btn').on('click', function (e) {
+        fetch('/list/imp', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                "id": id
             })
-        })//
-      
-        // $('#imp-btn').toggleClass('isImp');
-        if(res=='true')
+        })
+        if (res == 'true')
             $('#imp-btn').text('make imp')
         else
-        $('#imp-btn').text('remove imp')
+            $('#imp-btn').text('remove imp')
     })
-},  
-function(){
- $('#imp-btn').remove();
- $('#edit-btn').remove();
+},
+    function () {
+        $('#imp-btn').remove();
+        $('#edit-btn').remove();
 
-});
+    });
 
-function returnAndEmptyInput(){
-    const ele=document.querySelector(".add-new-task").value;
-    document.querySelector(".add-new-task").value=''
+function returnAndEmptyInput() {
+    const ele = document.querySelector(".add-new-task").value;
+    document.querySelector(".add-new-task").value = ''
     return ele
 }
-
-// function markImp(){
-// $('.imp-btn').click(function(event){
-
-// })
-// }
--- src/features/list/list.routes.js
+++ src/features/list/list.routes.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import ListController from "./list.controller.js";
 const ListRouter=new Router();
 ListRouter.get('/',ListController.get)
+//here u add the get one list items wrt id passed from client
 ListRouter.put('/',ListController.add)
 ListRouter.delete('/',ListController.delete);
 ListRouter.post('/',ListController.update);
